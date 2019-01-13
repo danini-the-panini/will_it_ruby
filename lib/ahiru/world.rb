700bc78848cb650aedb448314d523958c374d6aa
Daniel Smith <jellymann@gmail.com>
2019-01-13 17:08:20 +0200

    remove: old event logic

--- lib/ahiru/world.rb
+++ lib/ahiru/world.rb
@@ -8,30 +8,5 @@ module Ahiru
     def process_expression(sexp)
       @root_scope.process_expression(sexp)
     end
-
-    def fire_event(event, data = {})
-      blocks = @listeners[event]
-      return if blocks.nil?
-      blocks.each do |block|
-        block.call(data)
-      end
-    end
-
-    def add_listener(event, &block)
-      @listeners[event] ||= []
-      @listeners[event] << block
-      block
-    end
-
-    def remove_listener(event, block)
-      @listeners[event]&.delete(block)
-    end
   end
 end
-
-# events
-# class defined
-# module defined
-# method defined
-# constant defined
-# ivar/cvar/lvar/gvar defined
